145a947e6e97cb3d38f45972bdda653dd8605196
baisali609 <baisali609@gmail.com>
2025-02-11 08:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+	name: "John",
+	age: 22,
+	city: "abc"
+};
+
+function getKeys(obj){
+	return Object.keys(obj);
+}
+
+console.log(getKeys(student));
