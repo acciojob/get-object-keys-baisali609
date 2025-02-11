589999d63081db000dc5fe2114267aae48df6ab4
baisali609 <baisali609@gmail.com>
2025-02-11 11:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function getKeys(obj){
 }
 
 console.log(getKeys(student));
+
+
+cy.visit(baseUrl + "/main.html");
+cy.document().then(doc => {
+    const script = doc.querySelector('script[src="./script.js"]');
+    const scriptUrl = script.getAttribute("src");
+    cy.readFile(scriptUrl).then(jsCode => {
+        eval(jsCode);
+
+        const myObj = { name: "John", age: 30, city: "New York" };
+        const keys = getKeys(myObj); // FIXED: Call function directly
+        expect(keys).to.deep.equal(["name", "age", "city"]);
+    });
+});
